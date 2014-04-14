d1cc65b9522687880cbcf1c5d74de41cbb1db66d
Shawn Steinbrecher <steis6622@gmail.com>
2014-04-14 14:05:34 -0500

    Added a remember token field to the user table, so that we can utilize cookies.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414184112) do
+ActiveRecord::Schema.define(version: 20140414185047) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 20140414184112) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
