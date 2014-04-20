f6a947814a44700684a76fc4d0377516689cc129
Matt Schmitz <mtschmitz@wisc.edu>
2014-04-20 00:25:11 -0500

    prelim helpers to find users in area

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,16 @@ class ApplicationController < ActionController::Base
   def configure_devise_permitted_parameters
     registration_params = [:name, :email, :password, :password_confirmation]
 
-    if params[:action] == 'create'
+    
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(edit_params << :current_password)
+      }
+    elsif params[:action] == 'create'
       devise_parameter_sanitizer.for(:sign_up) { 
         |u| u.permit(registration_params) 
       }
     end
+    
   end
 end
