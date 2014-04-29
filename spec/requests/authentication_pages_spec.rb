d1a21fe6dc9fe69ba3eaf6f7c1396d80d6e75796
Shawn Steinbrecher <steis6622@gmail.com>
2014-04-29 16:02:24 -0500

    Added user login tests.

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "signin page" do
+    before { visit new_user_session_path }
+
+
+    describe "while not signed in" do
+      it { should_not have_link('Edit profile', href:  edit_user_registration_path) }
+      it { should_not have_link('Logout', href: destroy_user_session_path) }
+      it { should have_link('Login', href: new_user_session_path) }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+      
+      it { should have_title('Sign in') }
+      it { should have_error_message('Invalid email or password.') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_error_message('Invalid email or password.') }
+      end
+    end
+
+    describe "with valid information" do
+
+      before { sign_in user }
+
+     # it { should have_title(user.name) }
+      it { should have_link('Edit profile', href: edit_user_registration_path) }
+      it { should have_link('Logout', href: destroy_user_session_path) }
+      it { should_not have_link('Login', href: new_user_session_path) }
+
+      describe "followed by signout" do
+        before { click_link "Logout" }
+        it { should have_link('Login') }
+      end
+    end
+  end
+
+end
