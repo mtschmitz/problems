4de5f1af75638bb5c91c27c3d82dcd5105168168
Shawn Steinbrecher <steis6622@gmail.com>
2014-04-29 16:34:11 -0500

    Added another test helper to test alert divs

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,6 +9,12 @@ end
 	
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_alert_message do |message|
+  match do |page|
     expect(page).to have_selector('div.alert.alert-alert', text: message)
   end
 end
