4fade52359b477388abd1e83ab201e92d7d96e5d
Shawn Steinbrecher <steis6622@gmail.com>
2014-04-29 17:07:06 -0500

    Added tests to test problem creation.

--- spec/requests/problems_pages_spec.rb
+++ spec/requests/problems_pages_spec.rb
@@ -2,6 +2,47 @@ require 'spec_helper'
 
 describe "Problems" do
 
-  
+  subject { page }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:submit) { "Create Problem" }  
+
+  describe "posting new problems" do
+    describe "while not logged in" do
+      before { visit new_problem_path }
+      it { should have_title('Sign in') }
+      it { should have_alert_message('You need to sign in or sign up before continuing.') }
+    end    
+
+    describe "after logging in" do
+      before do
+        sign_in user 
+        visit new_problem_path
+      end
+
+      it { should have_title('New Problem') }
+
+#This test may not be needed
+#      it "should not create a problem post" do
+#        expect{ click_button submit }.to_not change(Problems, :count) 
+#      end
+
+      describe "with missing information" do
+        before { click_button submit }
+        it { should have_title('New Problem') }
+        it { should have_error_message('error') }
+      end
+      
+      describe "with valid information" do
+        before do
+          fill_in "problem_name",         with: "Forsit problem name"
+          fill_in "problem_description",  with: "Forsit problem description."
+          click_button submit
+        end
+        
+        it { should have_title('Problems') }
+      end      
+
+    end
+  end
 
 end
